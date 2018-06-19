e4e713ffb44a990bc13c1f17d0515796305bca29
James McCaughey <jmccaughey@kirbyvillecisd.org>
2018-06-19 15:01:43 -0500

    Added loading gif.

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
 		<header>
 			<h1>Network Monitor</h1>
 		</header>
-		<section id="tree_container"></section>
+		<section id="tree_container"><center><img src="loading.gif" /></center></section>
 	</body>
 </html>
