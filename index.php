c0c51559b5ce3d6d2135f6dd73d268a38cf9f881
James McCaughey <jmccaughey@kirbyvillecisd.org>
2018-06-19 14:53:19 -0500

    Added jquery-3.2.1.min.js.

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 	<head>
 		<title>Network Monitor</title>
 		<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+		<script src="jquery-3.2.1.min.js"></script>
 		<script src="javascript.js"></script>
 	</head>
 
 	<body onLoad="loadTree();">
+		<header>
+			<h1>Network Monitor</h1>
+		</header>
 		<section id="tree_container"></section>
 	</body>
 </html>
